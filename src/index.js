584d9d49b888f854a64a72d411254ba86e546d78
Klaudia Kieszkowska <claudiaandjazz@gmail.com>
2019-11-14 23:50:22 +0100

    Drawing with mouse :mouse:

--- src/index.js
+++ src/index.js
@@ -1,22 +1,96 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
 
-import styles from './styles.css'
+import './styles.css';
 
 export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+  constructor(props) {
+    super(props);
+    this.state = {
+      ctx: null,
+      drawing: false,
+      mousePos: {
+        x: 0,
+        y: 0,
+      },
+      lastPos: {
+        x: 0,
+        y: 0,
+      },
+    }
+    this.canvas = React.createRef();
+
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.renderCanvas = this.renderCanvas.bind(this);
+    this.start = this.start.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      ...this.state,
+      ctx: this.canvas.current.getContext('2d'),
+    });
+    this.clearCanvas();
+    this.start();
+  }
+
+  clearCanvas() {
+    // noinspection SillyAssignmentJS
+    this.canvas.current.width = this.canvas.current.width;
+  }
+
+  handleMouseDown(e) {
+    this.setState({
+      ...this.state,
+      drawing: true,
+      lastPos: this.getMousePos(e),
+    });
+  }
+
+  handleMouseUp(e) {
+    this.setState({
+      ...this.state,
+      drawing: false,
+      lastPos: this.getMousePos(e),
+    });
+  }
+
+  handleMouseMove(e) {
+    this.setState({
+      ...this.state,
+      mousePos: this.getMousePos(e),
+    })
+  }
+
+  getMousePos(mouseEvent) {
+    let rect = this.canvas.current.getBoundingClientRect();
+    return {
+      x: mouseEvent.clientX - rect.left,
+      y: mouseEvent.clientY - rect.top
+    };
+  }
+
+  renderCanvas() {
+    if (this.state.drawing) {
+      this.state.ctx.moveTo(this.state.lastPos.x, this.state.lastPos.y);
+      this.state.ctx.lineTo(this.state.mousePos.x, this.state.mousePos.y);
+      this.state.ctx.stroke();
+      this.setState({
+        ...this.state,
+        lastPos: this.state.mousePos,
+      });
+    }
+  }
+
+  start() {
+    requestAnimationFrame(this.start);
+    this.renderCanvas();
   }
 
   render() {
-    const {
-      text
-    } = this.props
-
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
+      <canvas ref={this.canvas} onMouseDown={this.handleMouseDown} onMouseMove={this.handleMouseMove} onMouseUp={this.handleMouseUp}/>
     )
   }
 }
